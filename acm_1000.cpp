32b2995e4bfec38b49387915d9b35d4aa4f81dfd
raidenluikang <raidenluikang@gmail.com>
2016-12-14 11:40:12 +0500

    acm.timus.ru 1000 problem solution.

---
+++ acm_1000.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(int argc, char* argv[])
+{
+   std::ios::sync_with_stdio(false); std::cin.tie(0);std::cout.tie(0);
+   int a,b;
+   std::cin >> a >> b;
+   std::cout << a + b << '\n';
+}
